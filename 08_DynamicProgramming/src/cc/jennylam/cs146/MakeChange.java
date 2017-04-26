10125d738bcd0386f3614a94953c6a391175df22
Jenny Lam <jylam@ucla.edu>
2017-04-26 09:20:38 -0700

    Initial import

---
+++ 08_DynamicProgramming/src/cc/jennylam/cs146/MakeChange.java
@@ -0,0 +1,18 @@
+package cc.jennylam.cs146;
+
+public class MakeChange {
+
+    public static void main(String[] args) {
+        // your test code
+    }
+
+    public static int BruteForceMakeChange(int[] denominations, int target) {
+        // optional, but a good idea to have a working solution to test against.
+        return 0;
+    }
+
+    public static int DynamicProgrammingMakeChange(int[] denominations, int target) {
+        // TODO: implement a DP version of MakeChange
+        return 0;
+    }
+}
